a26501a393fc5e621247a69bd88f9405893d4b30
genndy007 <kochevgenazp@gmail.com>
2021-11-25 14:38:50 +0200

    Create basic example of twitter api usage

--- src/twitter/example.js
+++ src/twitter/example.js
@@ -1,3 +1,14 @@
-const getTweets = require('recent-tweets')
+// working last 10 tweets
+// curl -X GET -H "Authorization: Bearer <BEARERTOKEN>" "https://api.twitter.com/2/users/152987149/tweets"
 
-getTweets('BBCWorld').then(x => console.log(x))
+require('dotenv').config()
+const { TwitterApi } = require('twitter-api-v2')
+
+const twitterClient = new TwitterApi(process.env.TWITTER_BEARER_TOKEN)
+
+const roClient = twitterClient.readOnly
+const userId = '152987149'
+roClient.v2.userTimeline(userId).then(res => {
+    const all_tweets = res.data.data
+    all_tweets.forEach(el => console.log(el.text + '\n\n'))
+})
