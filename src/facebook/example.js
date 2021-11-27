945888f0fb977a3e5d866bb4b29931e3da2eda45
Andrey Vlasov <andrey.vlasov02@gmail.com>
2021-11-27 21:08:44 +0200

    Create function to get HTML of FB posts by URL

---
+++ src/facebook/example.js
@@ -0,0 +1,28 @@
+const rp = require('request-promise-native')
+const cheerio = require('cheerio')
+
+function GetFbPosts(pageUrl) {
+    const requestOptions = {
+        url: pageUrl,
+        headers: {
+            'User-Agent':
+                'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0',
+        },
+    }
+    return rp.get(requestOptions).then(postsHtml => {
+        const $ = cheerio.load(postsHtml)
+        const timeLinePostEls = $('.userContent')
+            .map((i, el) => $(el))
+            .get()
+        const posts = timeLinePostEls.map(post => {
+            return {
+                message: post.html(),
+                createdAt: post
+                    .parents('.userContentWrapper')
+                    .find('.timestampContent')
+                    .html(),
+            }
+        })
+        return posts
+    })
+}
