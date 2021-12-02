ad360b8f15c5532e8d0bf3c10bff81c002415ce4
genndy007 <kochevgenazp@gmail.com>
2021-12-03 00:41:20 +0200

    Add sql for testing data in tables

---
+++ src/db/test_data/add.sql
@@ -0,0 +1,18 @@
+insert into social_network (name) values 
+('telegram'),
+('tumblr'),
+('facebook'),
+('twitter');
+
+
+insert into source (name, link, rubric, social_network_id) values
+('tsn', '823uivjoj23', 'politics', 1),
+('cnn', '189uj3i2fmji00', 'news', 2),
+('espn', '17u98j3fnio', 'sports', 3),
+('culturetv', '183j9j2jv9mio', 'music', 4);
+
+insert into notes (link, headline, content, created_at, source_id) values
+('8193f9fjwfsldk', 'lololol', 'ji2ejf0wdojivjmwkmfdkmwkef', '1999-01-08 04:05:06', 1),
+('1u3fhj9o23ficm', 'lalallala', 'u293hfiunwidnkjnsvd', '1999-01-10 04:05:06', 2),
+('013jr9ij0923f', 'trolollo', '82jf9ej92j3f9j', '1999-01-14 04:05:06', 3),
+('293902je0weovik', 'opopopopopo', '283fjj2oeijwdklvm', '1999-01-17 04:05:06', 4);
