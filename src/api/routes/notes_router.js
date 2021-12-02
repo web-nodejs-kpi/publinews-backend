0495f017ea283c92d08634338303383272a221d6
genndy007 <kochevgenazp@gmail.com>
2021-12-02 16:18:35 +0200

    Fix news router

--- src/api/routes/notes_router.js
+++ src/api/routes/notes_router.js
@@ -2,21 +2,20 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('all notes')
-    res.sendStatus(200)
+    res.status(200).send('all notes')
 })
 
 router.get('/:note_id', (req, res) => {
-    res.send(`note with id ${req.params.note_id}`)
-    res.sendStatus(200)
+    res.status(200).send(`note with id ${req.params.note_id}`)
 })
 
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.sendStatus(201)
+    res.status(201).send(req.body)
 })
 
 router.delete('/:note_id', (req, res) => {
-    console.log(`deleted note with id ${req.params.note_id}`)
-    res.sendStatus(204)
+    res.status(204).send(`deleted note with id ${req.params.note_id}`)
 })
+
+module.exports = router
