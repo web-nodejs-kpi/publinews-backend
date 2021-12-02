d9c70320cc22317f40324466cb85c4791aad6e68
genndy007 <kochevgenazp@gmail.com>
2021-12-02 15:59:51 +0200

    Add news router

---
+++ src/api/routes/news_router.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send(`request news with params ${req.params}`)
+    res.sendStatus(200)
+})
