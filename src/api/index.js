e272480dc679d5f2a2f8767b0779f5edbf8078ec
genndy007 <kochevgenazp@gmail.com>
2021-12-02 16:26:46 +0200

    Fix route news

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,12 @@ const app = express()
 require('dotenv').config()
 
 const notes_router = require('./routes/notes_router')
+const sources_router = require('./routes/sources_router')
+const news_router = require('./routes/news_router')
 
 app.use('/notes', notes_router)
+app.use('/sources', sources_router)
+app.use('/news', news_router)
 
 app.get('/', (req, res) => {
     res.send('hello world')
