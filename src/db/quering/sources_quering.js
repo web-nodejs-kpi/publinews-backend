36691ac311acb29c92658d89008bd6cc6812957d
genndy007 <kochevgenazp@gmail.com>
2021-12-04 16:20:27 +0200

    Fix catching errors in source quering

--- src/db/quering/sources_quering.js
+++ src/db/quering/sources_quering.js
@@ -16,20 +16,30 @@ const get_all_sources = async () => {
 }
 
 const insert_source = async (source_name, link, rubric, social_network_id) => {
-    const source_id = await knex(TABLE_NAME)
-        .insert({
-            name: source_name,
-            link: link,
-            rubric: rubric,
-            social_network_id: social_network_id,
-        })
-        .returning('source_id')
-    return source_id[0]
+    try {
+        const source_id = await knex(TABLE_NAME)
+            .insert({
+                name: source_name,
+                link: link,
+                rubric: rubric,
+                social_network_id: social_network_id,
+            })
+            .returning('source_id')
+        return source_id[0]
+    } catch {
+        return new Error('cannot insert such info')
+    }
 }
 
 const delete_source = async source_id => {
-    const deleted = await knex(TABLE_NAME).where('source_id', source_id).del()
-    return deleted
+    try {
+        const deleted = await knex(TABLE_NAME)
+            .where('source_id', source_id)
+            .del()
+        return deleted
+    } catch {
+        return new Error(`cannot delete with id ${source_id}`)
+    }
 }
 
 module.exports = {
