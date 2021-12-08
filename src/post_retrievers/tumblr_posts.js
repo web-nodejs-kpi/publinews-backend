129ce3a72282e741972839af2eb3712d4886ea2e
Andrey Vlasov <andrey.vlasov02@gmail.com>
2021-12-08 21:29:44 +0200

    Implement asyncronicity in tumblr retriever module

--- src/post_retrievers/tumblr_posts.js
+++ src/post_retrievers/tumblr_posts.js
@@ -3,12 +3,14 @@ const tumblr = require('tumblr.js')
 
 const client = tumblr.createClient({
     consumer_key: process.env.TUMBLR_CONSUMER_KEY,
+    returnPromises: true,
 })
 
-client.blogPosts('taylorswift', (err, resp) => {
-    console.log(
-        resp.posts.slice(0, 2).map(el => {
-            return { content: el.reblog.comment, date: el.date }
-        })
-    )
-})
+const get_tumblr_posts = async (blogName, postsCount = 3) => {
+    const res = await client.blogPosts(blogName)
+    return res.posts.slice(0, postsCount).map(el => {
+        return { content: el.reblog.comment, date: el.date }
+    })
+}
+
+module.exports = get_tumblr_posts()
