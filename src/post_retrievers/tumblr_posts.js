e4637cec6d3fb8c6191046dfdf480218c82d0c4b
Andrey Vlasov <andrey.vlasov02@gmail.com>
2021-12-08 20:29:11 +0200

    Add transmutation

--- src/post_retrievers/tumblr_posts.js
+++ src/post_retrievers/tumblr_posts.js
@@ -1,8 +1,14 @@
+require('dotenv').config()
 const tumblr = require('tumblr.js')
+
 const client = tumblr.createClient({
     consumer_key: process.env.TUMBLR_CONSUMER_KEY,
 })
 
-client.blogPosts('taylorswift', function (err, resp) {
-    console.log(resp.posts)
+client.blogPosts('taylorswift', (err, resp) => {
+    console.log(
+        resp.posts.slice(0, 2).map(el => {
+            return { content: el.reblog.comment, date: el.date }
+        })
+    )
 })
