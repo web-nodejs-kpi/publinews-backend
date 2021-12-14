9821659910919526c4ef4eadc0d0f6c74fc4a33e
genndy007 <kochevgenazp@gmail.com>
2021-12-14 23:21:29 +0200

    Make tumblr work

--- src/post_retrievers/tumblr_posts.js
+++ src/post_retrievers/tumblr_posts.js
@@ -6,11 +6,20 @@ const client = tumblr.createClient({
     returnPromises: true,
 })
 
-const get_tumblr_posts = async (blogName, postsCount = 3) => {
+const get_tumblr_posts = async (blogName, postsCount = 5) => {
     const res = await client.blogPosts(blogName)
+
     return res.posts.slice(0, postsCount).map(el => {
-        return { content: el.reblog.comment, date: el.date }
+        return {
+            link: el.post_url,
+            headline: el.caption.slice(0, 50) + '...',
+            content: el.caption,
+            date: el.date,
+        }
     })
 }
 
-module.exports = get_tumblr_posts()
+module.exports = get_tumblr_posts
+
+// Example of usage
+// get_tumblr_posts('taylorswift').then(res => console.log(res))
