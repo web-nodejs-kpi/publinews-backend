fcd31214b02446b9360f18e5c8c98d91a181671b
genndy007 <kochevgenazp@gmail.com>
2021-12-15 00:52:57 +0200

    Create news aggregation functionality

---
+++ src/post_retrievers/index.js
@@ -0,0 +1,50 @@
+const get_facebook_posts = require('./facebook_posts')
+const get_tumblr_posts = require('./tumblr_posts')
+const get_twitter_posts = require('./twitter_posts')
+
+const {
+    get_all_sources,
+    find_source_social_network,
+} = require('../db/quering/sources_quering')
+
+const FACEBOOK_SOURCE_NAME = 'fb'
+const TUMBLR_SOURCE_NAME = 'tu'
+const TWITTER_SOURCE_NAME = 'tw'
+
+const get_posts_from_social_network = async source => {
+    const link = source.link
+    const source_id = source.source_id
+
+    let social_network = ''
+    try {
+        social_network = await find_source_social_network(source_id)
+    } catch {
+        return []
+    }
+
+    switch (social_network) {
+        case FACEBOOK_SOURCE_NAME:
+            return await get_facebook_posts(link)
+        case TUMBLR_SOURCE_NAME:
+            return await get_tumblr_posts(link)
+        case TWITTER_SOURCE_NAME:
+            return await get_twitter_posts(link)
+    }
+}
+
+const get_recent_news = async () => {
+    const sources = await get_all_sources()
+
+    const all_news = []
+    for (const source of sources) {
+        const posts = await get_posts_from_social_network(source)
+        all_news.push(...posts)
+    }
+    // return shuffled array
+    return all_news.sort((a, b) => 0.5 - Math.random())
+}
+
+module.exports = get_recent_news
+
+// Example of usage
+// get_recent_news().then(res => console.log(res))
