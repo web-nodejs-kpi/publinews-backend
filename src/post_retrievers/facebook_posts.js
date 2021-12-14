154cef64b4ef9ca278d113d3573054aa7bbb443d
genndy007 <kochevgenazp@gmail.com>
2021-12-14 23:24:38 +0200

    Make facebook work

--- src/post_retrievers/facebook_posts.js
+++ src/post_retrievers/facebook_posts.js
@@ -1,7 +1,7 @@
 const rp = require('request-promise-native')
 const cheerio = require('cheerio')
 
-const get_facebook_posts = async (username, posts_count = 3) => {
+const get_facebook_posts = async (username, posts_count = 5) => {
     const request_options = {
         url: 'https://www.facebook.com/' + username + '/posts',
         headers: {
@@ -11,20 +11,25 @@ const get_facebook_posts = async (username, posts_count = 3) => {
     }
     const data = await rp.get(request_options)
     const $ = cheerio.load(data)
+
     const timeline = $('.userContent')
         .map((i, el) => $(el))
         .get()
-    return timeline
-        .map(post => {
-            return {
-                text: post.text(),
-                created_at: post
-                    .parents('.userContentWrapper')
-                    .find('abbr')
-                    .attr('data-tooltip-content'),
-            }
-        })
-        .slice(Math.min(posts_count, 19))
+
+    return timeline.slice(0, posts_count).map(post => {
+        return {
+            link: request_options.url,
+            headline: post.text().slice(0, 50) + '...',
+            content: post.text(),
+            created_at: post
+                .parents('.userContentWrapper')
+                .find('abbr')
+                .attr('data-tooltip-content'),
+        }
+    })
 }
 
-module.exports = get_facebook_posts()
+module.exports = get_facebook_posts
+
+// Example of usage
+// get_facebook_posts('tsn.ua').then(res => console.log(res))
