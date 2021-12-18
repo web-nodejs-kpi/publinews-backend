ede5f9584e73741f16ba4fc09ed5c128af112d56
genndy007 <kochevgenazp@gmail.com>
2021-12-19 00:57:23 +0200

    Add news rubric routing

--- src/routes/news_router.js
+++ src/routes/news_router.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
-const get_recent_news = require('../post_retrievers')
+const {
+    get_recent_news,
+    get_recent_news_by_rubric,
+} = require('../post_retrievers')
 
 router.get('/', async (req, res) => {
     const news = await get_recent_news()
+
+    res.status(200).send(news)
+})
+
+router.get('/:rubric', async (req, res) => {
+    const rubric = req.params.rubric
+    const news = await get_recent_news_by_rubric(rubric)
 
     res.status(200).send(news)
 })
