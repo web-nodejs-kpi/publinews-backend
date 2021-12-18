58bf3db8fee8e13d3782964bc1bd011410ad47a8
genndy007 <kochevgenazp@gmail.com>
2021-12-19 00:21:37 +0200

    Add route for social networks

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 const notes_router = require('./routes/notes_router')
 const sources_router = require('./routes/sources_router')
 const news_router = require('./routes/news_router')
+const social_networks_router = require('./routes/social_networks_router')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -14,6 +15,7 @@ app.use(cors({ origin: `http://localhost:3000` }))
 app.use('/notes', notes_router)
 app.use('/sources', sources_router)
 app.use('/news', news_router)
+app.use('/social_networks', social_networks_router)
 
 app.get('/', (req, res) => {
     res.send('hello world')
